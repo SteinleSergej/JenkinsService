01bf9798de909fd793bb3a3f72fb15657443bdbf
SteinleSergej <steinle.sergej.prog@gmail.com>
2023-03-02 23:04:02 +0100

    Init

---
+++ JenkinsService/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JenkinsService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
